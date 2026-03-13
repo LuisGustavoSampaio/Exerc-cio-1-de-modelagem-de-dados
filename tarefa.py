97c7efc4bd321e323c428264461ef2be2c3e3b1a
Luís Gustavo <57230668+LuisGustavoSampaio@users.noreply.github.com>
2026-03-13 12:12:55 -0300

    Update tarefa.py
    criando todas as classes e funções

--- tarefa.py
+++ tarefa.py
@@ -1,5 +1,66 @@
 class Historico:
-    def __init__histórico(self):
-    self.transacoes = []
-    def adicionar transacao(self, transacao):
-    self.transacoes.append(transacao)
+    def __init__(self):
+        self.transacoes = []
+
+    def adicionar_transacao(self, transacao):
+        self.transacoes.append(transacao)
+
+
+class Conta:
+    def __init__(self, cliente, numero):
+        self.saldo = 0
+        self.numero = numero
+        self.agencia = "0001"
+        self.cliente = cliente
+        self.historico = Historico()
+
+    def sacar(self, valor):
+        if valor > self.saldo:
+            print("Saldo insuficiente")
+            return False
+
+        self.saldo -= valor
+        return True
+
+    def depositar(self, valor):
+        self.saldo += valor
+        return True
+
+class ContaCorrente(Conta):
+    def __init__(self, cliente, numero, limite=500, limite_saques=3):
+        super().__init__(cliente, numero)
+        self.limite = limite
+        self.limite_saques = limite_saques
+
+class Cliente:
+    def __init__(self, endereco):
+        self.endereco = endereco
+        self.contas = []
+
+    def adicionar_conta(self, conta):
+        self.contas.append(conta)
+
+    def __init__(self, nome, cpf, data_nascimento, endereco):
+        super().__init__(endereco)
+        self.nome = nome
+        self.cpf = cpf
+        self.data_nascimento = data_nascimento
+
+class Transacao:
+    def registrar(self, conta):
+        pass
+
+class Deposito(Transacao):
+    def __init__(self, valor):
+        self.valor = valor
+
+    def registrar(self, conta):
+        conta.depositar(self.valor)
+
+class Saque(Transacao):
+    def __init__(self, valor):
+        self.valor = valor
+
+    def registrar(self, conta):
+        conta.sacar(self.valor)
+
